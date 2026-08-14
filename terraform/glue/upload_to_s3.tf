90b044c2c131a9c569bca0eed79dd061bdf62167
Prakhar-ui <70754872+Prakhar-ui@users.noreply.github.com>
2026-08-14 10:13:37 +0530

    feat: upload trend intelligence Glue script

--- terraform/glue/upload_to_s3.tf
+++ terraform/glue/upload_to_s3.tf
@@ -17,3 +17,13 @@ resource "aws_s3_object" "silver_to_gold_analytics_glue_script" {
 
   etag = filemd5("${path.module}/scripts/silver_to_gold_analytics.py")
 }
+
+resource "aws_s3_object" "trend_intelligence_glue_script" {
+  bucket = format("%s-bronze-%s", local.name_prefix, local.account_id)
+
+  key = "glue/scripts/trend_intelligence.py"
+
+  source = "${path.module}/scripts/trend_intelligence.py"
+
+  etag = filemd5("${path.module}/scripts/trend_intelligence.py")
+}
